a1450f21ed156faa6b1e48aa33ea5107d60d983c
Debtanu Maitra <maitradebtanu@gmail.com>
2023-05-13 16:41:06 +0530

    Updated Code

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((student) => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((student) => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
-}
+    const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+    arr.push(newStudent);
+    console.log(arr);
+  }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter((student) => student.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    { id: 4, name: "Debu", age: "27", marks: 92 },
+    { id: 5, name: "Koustav", age: "29", marks: 87 },
+    { id: 6, name: "Tammy", age: "26", marks: 95 },
+  ];
+
+  arr = arr.concat(newArr);
+  console.log(arr);
 }
